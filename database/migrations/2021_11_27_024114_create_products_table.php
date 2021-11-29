9d9f92eb837d12c0b6a486cefc2b06fa52a646db
Alex <alex95.taylor@gmail.com>
2021-11-28 22:29:33 -0500

    Add category column to proudcts table migration

--- database/migrations/2021_11_27_024114_create_products_table.php
+++ database/migrations/2021_11_27_024114_create_products_table.php
@@ -16,6 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->enum('category', array('Office Supplies', 'Electronics', 'Appliances'));
             $table->text('description');
             $table->decimal('price');
             $table->text('picture_url');
